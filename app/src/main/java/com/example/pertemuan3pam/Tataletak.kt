a663b651dd81e347d6a65b8def1c035637ad1a23
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-17 16:08:42 +0700

    TataletakBoxColumnRowvalgambar

--- app/src/main/java/com/example/pertemuan3pam/Tataletak.kt
+++ app/src/main/java/com/example/pertemuan3pam/Tataletak.kt
@@ -1,16 +1,20 @@
 package com.example.pertemuan3pam
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import kotlin.coroutines.coroutineContext
 
@@ -85,6 +89,22 @@ fun TataletakRowColumn(modifier: Modifier) {
             Text(text = "Komponen1Kolom2")
             Text(text = "Komponen2Kolom2")
             Text(text = "Komponen3Kolom2")
+        }
+    }
+}
+
+@Composable
+fun TataletakBoxColumnRow(modifier: Modifier) {
+    val gambar = painterResource(id = R.drawable.nbil)
+    Column {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 110.dp)
+                .background(color = Color.DarkGray),
+            contentAlignment = Alignment.Center
+        ) {
+
         }
     }
 }
